c73420a95b72d97ad48d5c98d0ad867cac0e16e7
gregaswright <greg.a.s.wright@gmail.com>
2020-10-25 09:55:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// const writeCards = [ 'Lisa', 'Kaitlin', 'Jan' ]
+
+function writeCards(array, surprise) {
+    let cards = []
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return cards
+}
+
+function countDown(num) {
+   while (0 < num) {
+       console.log(num);
+       num -= 1;
+   }
+   console.log(num)
+}
